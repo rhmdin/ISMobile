ebc6a0e25c7a70582b05ba31029b60d412bcd4ee
dina <2011522012_Rahmadina@student.unand.ac.id>
2022-11-21 09:23:25 +0700

    add layout mafa

---
+++ ISMobile/app/src/main/java/com/example/ismobile/activity/FormBatalTaActivity.java
@@ -0,0 +1,16 @@
+package com.example.ismobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ismobile.R;
+
+public class FormBatalTaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_batal_ta);
+    }
+}
---
+++ ISMobile/app/src/main/java/com/example/ismobile/activity/SidangTaActivity.java
@@ -0,0 +1,16 @@
+package com.example.ismobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ismobile.R;
+
+public class SidangTaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sidang_ta);
+    }
+}
