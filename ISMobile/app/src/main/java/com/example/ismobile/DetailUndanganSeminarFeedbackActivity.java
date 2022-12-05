b67b99070a4d3f9d71a5a43dc3ec07508379031c
dina <2011522012_Rahmadina@student.unand.ac.id>
2022-12-05 15:29:02 +0700

    detail undangan

---
+++ ISMobile/app/src/main/java/com/example/ismobile/DetailUndanganSeminarFeedbackActivity.java
@@ -0,0 +1,14 @@
+package com.example.ismobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailUndanganSeminarFeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_undangan_seminar_feedback);
+    }
+}
