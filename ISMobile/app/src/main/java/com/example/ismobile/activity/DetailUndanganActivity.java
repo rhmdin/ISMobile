2e76820fa1768ecb9513ea0a636be30f5590da54
dina <2011522012_Rahmadina@student.unand.ac.id>
2022-12-05 19:22:55 +0700

    detail sidang

--- ISMobile/app/src/main/java/com/example/ismobile/activity/DetailUndanganActivity.java
+++ ISMobile/app/src/main/java/com/example/ismobile/activity/DetailUndanganActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ismobile.R;
 
@@ -22,5 +25,24 @@ public class DetailUndanganActivity extends AppCompatActivity {
         String kategori = undangandetail.getStringExtra("undangan_kategori");
         detail_kategori.setText(kategori);
         detail_judul_kategori.setText("Detail "+kategori);
+
+        Button btn_acc = (Button) findViewById(R.id.detailjadwal_btn_acc);
+        Intent feedbacksemhas = new Intent(DetailUndanganActivity.this, DetailUndanganSeminarActivity.class);
+        Intent feedbacksidang = new Intent(DetailUndanganActivity.this, DetailUndanganSidangActivity.class);
+        btn_acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int b =1;
+                String a = kategori;
+                Toast.makeText(DetailUndanganActivity.this, kategori, Toast.LENGTH_SHORT).show();
+                if(a == "Seminar Hasil"){
+                    startActivity(feedbacksemhas);
+                }
+                else if(a == "Sidang TA"){
+                    startActivity(feedbacksidang);
+                }
+            };
+
+    });
     }
 }
