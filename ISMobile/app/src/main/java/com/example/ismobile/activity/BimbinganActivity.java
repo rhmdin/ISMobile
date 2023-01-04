d74c1543fdb1cbc53836e6d19b312a4c6cbb8816
dina <89837465+rhmdin@users.noreply.github.com>
2023-01-04 12:45:30 +0700

    .

---
+++ ISMobile/app/src/main/java/com/example/ismobile/activity/BimbinganActivity.java
@@ -0,0 +1,16 @@
+package com.example.ismobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ismobile.R;
+
+public class BimbinganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bimbingan);
+    }
+}
